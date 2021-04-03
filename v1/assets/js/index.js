1a645bd1a10315589320520f501c7ee6f590bea8
ROULOIS Ewen <rouloisewen@gmail.com>
2021-04-03 19:37:50 +0200

    Remove parcel

--- v1/assets/js/index.js
+++ v1/assets/js/index.js
@@ -1,1 +1,11 @@
+import 'regenerator-runtime/runtime'
+
 console.log("Test");
+const sendMailForm = document.querySelector('#sendMailForm');
+const buttonSendMailForm = sendMailForm.querySelector('button[type="submit"]');
+
+buttonSendMailForm.addEventListener('click', async function(e){
+    e.preventDefault();
+    const response = await fetch('http://localhost:8080/api/mail');
+    console.log(await response.json())
+})
