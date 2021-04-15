7e2b7e13737ae6e427b7dff944e51c4269785981
ROULOIS Ewen <rouloisewen@gmail.com>
2021-04-15 18:32:55 +0200

    Add JS

--- v1/assets/js/index.js
+++ v1/assets/js/index.js
@@ -1,11 +1,49 @@
-import 'regenerator-runtime/runtime'
+function getPageScroll() {
+  var yScroll;
 
-console.log("Test");
-const sendMailForm = document.querySelector('#sendMailForm');
-const buttonSendMailForm = sendMailForm.querySelector('button[type="submit"]');
+  if (window.pageYOffset) {
+    yScroll = window.pageYOffset;
+  } else if (document.documentElement && document.documentElement.scrollTop) {
+    yScroll = document.documentElement.scrollTop;
+  } else if (document.body) {
+    yScroll = document.body.scrollTop;
+  }
+  return yScroll;
+}
 
-buttonSendMailForm.addEventListener('click', async function(e){
+function getCurrentURL() {
+  return window.location.port
+    ? `${window.location.protocol}//${window.location.hostname}:${window.location.port}${window.location.pathname}`
+    : `${window.location.protocol}//${window.location.hostname}${window.location.pathname}`;
+}
+
+let navigationMenu = document.querySelector("#navigationMenu");
+let scrollTo = document.querySelectorAll(".scrollTo");
+
+scrollTo.forEach((link) => {
+  let href = link.getAttribute("data-to");
+  link.addEventListener("click", function (e) {
     e.preventDefault();
-    const response = await fetch('http://localhost:8080/api/mail');
-    console.log(await response.json())
-})
+    let element = document.querySelector(href);
+    console.log(element);
+    element.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
+});
+
+navigationMenu.querySelectorAll("a").forEach((link) => {
+  let href = link.href.substring(getCurrentURL().length);
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    let element = document.querySelector(href);
+    console.log(element);
+    element.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
+});
+
+console.log(getCurrentURL());
